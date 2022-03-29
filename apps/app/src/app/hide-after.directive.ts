265680fef11750d2d085a51e58b57deab708b230
NAM NGUYEN <hoangnamnguyen191@gmaiil.com>
2022-03-29 01:20:23 -0400

    feat: add context

--- apps/app/src/app/hide-after.directive.ts
+++ apps/app/src/app/hide-after.directive.ts
@@ -6,15 +6,38 @@ import {
   ViewContainerRef,
 } from '@angular/core';
 
+class HideAfterContext {
+  public get $implicit() {
+    return this.angularStructuralDirectiveHideAfter;
+  }
+  // need to match the directive input for the "as" to work
+  public angularStructuralDirectiveHideAfter = 1000;
+  public counter = 0;
+}
+
 @Directive({
   selector: '[angularStructuralDirectiveHideAfter]',
 })
 export class HideAfterDirective implements OnInit {
+  private _delay = 0;
+
   @Input('angularStructuralDirectiveHideAfter')
-  delay = 0;
+  set delay(newDelay: number | null) {
+    if (newDelay) {
+      this._delay = newDelay;
+      this.context.counter = newDelay / 1000;
+      this.context.angularStructuralDirectiveHideAfter = newDelay / 1000;
+    }
+  }
+
+  get delay(): number {
+    return this._delay;
+  }
 
   @Input('angularStructuralDirectiveHideAfterThen')
   placeHolder: TemplateRef<any> | null = null;
+
+  private context = new HideAfterContext();
 
   constructor(
     private viewContainerRef: ViewContainerRef,
@@ -22,11 +45,19 @@ export class HideAfterDirective implements OnInit {
   ) {}
 
   ngOnInit(): void {
-    this.viewContainerRef.createEmbeddedView(this.templateRef);
+    this.viewContainerRef.createEmbeddedView(this.templateRef, this.context);
+
+    const intervalId = setInterval(() => {
+      this.context.counter--;
+    }, 1000);
     setTimeout(() => {
       this.viewContainerRef.clear();
+      clearInterval(intervalId);
       if (this.placeHolder) {
-        this.viewContainerRef.createEmbeddedView(this.placeHolder);
+        this.viewContainerRef.createEmbeddedView(
+          this.placeHolder,
+          this.context
+        );
       }
     }, this.delay);
   }
